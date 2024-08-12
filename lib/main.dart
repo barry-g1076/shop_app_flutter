fb5c9df4e5fc01cd7ba9f0c7743d6c32a9cb8aef
Barrington Patterson <barry545young@gmail.com>
2024-08-11 21:10:00 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/cart_provider.dart';
+import 'package:shop_app_flutter/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        title: 'Shopping App',
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(254, 206, 1, 1),
+            primary: const Color.fromRGBO(254, 206, 1, 1),
+          ),
+          appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          )),
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            bodySmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
